575efb9fe791cd20d9e5028bdca9376afafa3ebb
rangexMitchell <mitchell@rangex.golf>
2022-04-11 13:46:51 +0900

    리액트 프로젝트 생성 / 웹뷰띄우기 테스트

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          이곳에서 웹뷰 동작합니다.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
